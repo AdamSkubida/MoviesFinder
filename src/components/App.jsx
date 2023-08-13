f15f574db64175d5702107adc023e72f2e5cf187
AdamSkubida <adasiow.s@gmail.com>
2023-08-13 21:11:35 +0200

    App/fetchMovies function added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,26 @@
+import { useEffect } from 'react';
+
 export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+  const apiKey = 'c6cec78029d9b3a5ee3a2d24ebcb1c0f';
+  const apiUrl = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}`;
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+      try {
+        const response = await fetch(apiUrl);
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        console.log(data.results);
+        return data.results;
+      } catch (error) {
+        console.error('Wystąpił błąd:', error);
+        return [];
+      }
+    };
+    fetchMovies();
+  }, [apiUrl]);
+
+  return <div>LOL</div>;
 };
